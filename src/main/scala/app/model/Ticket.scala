53cdd3033da82908fa827886a0e444b157bbbe4e
kobtea <kobtea9696@gmail.com>
2014-10-10 20:19:36 +0900

    impl `Issue` and `Bug`

--- src/main/scala/app/model/Ticket.scala
+++ src/main/scala/app/model/Ticket.scala
@@ -1,7 +1,19 @@
 package app.model
 
-trait Ticket {
+// MEMO: `sealed`をつけると`Ticket`をmixinできるのはこのファイル内だけ
+sealed trait Ticket {
   val id: Long
   val title: String
   val status: TicketStatus
 }
+
+case class Issue(id: Long,
+                 title: String,
+                 status: TicketStatus = TicketStatus.Open) extends Ticket
+
+case class Bug(// MEMO: デフォルトだとpublicなvalになる
+               id: Long,
+               title: String,
+               status: TicketStatus = TicketStatus.Open,
+               // MEMO: private val hoge: String, みたいな書き方もできる
+               description: String) extends Ticket
